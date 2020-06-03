115334c9955edf9a85455bcaf06a2bd14492d70e
Ivan <sivan9458@gmail.com>
2020-06-04 01:34:58 +0700

    Create loseCondition function

--- src/hangman.h
+++ src/hangman.h
@@ -3,5 +3,6 @@
 void hangman(int n);
 int mainMenuPrint();
 int victoryCondition(int guessChar, char* word);
+int loseCondition(int guessErr, char* word);
 int createSword(int sizeWord, char* secretWord);
 #endif
