61332374112c870d6dd839a7f65d5d9d0c88316c
Alesha27 <aleksey27.00@mail.ru>
2020-06-03 17:33:02 +0700

    Create hangman function

--- src/hangman.c
+++ src/hangman.c
@@ -1,1 +1,36 @@
 #include "hangman.h"
+#include <stdio.h>
+
+void hangman(int n)
+{
+    switch (n) {
+    case 6:
+        printf(" \n\n   HANGMAN \n\n +=========+\n  | \t||\n  O \t||\n "
+               "|||\t||\n  | \t||\n | |\t||\n | |\t||\n +=========+\n");
+        break;
+    case 5:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n "
+               "|||\t||\n  | \t||\n | |\t||\n | |\t||\n +=========+\n");
+        break;
+    case 4:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n    "
+               "\t||\n  | \t||\n | |\t||\n | |\t||\n +=========+\n");
+        break;
+    case 3:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n    "
+               "\t||\n    \t||\n | |\t||\n | |\t||\n +=========+\n");
+        break;
+    case 2:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n    "
+               "\t||\n    \t||\n    \t||\n | |\t||\n +=========+\n");
+        break;
+    case 1:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n    "
+               "\t||\n    \t||\n    \t||\n    \t||\n +=========+ \n");
+        break;
+    case 0:
+        printf(" \n\n   HANGMAN \n\n +=========+\n    \t||\n    \t||\n    "
+               "\t||\n    \t||\n    \t||\n    \t||\n +=========+\n");
+        break;
+    }
+}
--- src/hangman.h
+++ src/hangman.h
@@ -1,3 +1,4 @@
 #ifndef HANGMAN_H
 #define HANGMAN_H
+void hangman(int n);
 #endif
