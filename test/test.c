4a6cbe2f679d6622c6c4e00722c3a61f62a675bc
Alesha27 <aleksey27.00@mail.ru>
2020-06-03 17:49:36 +0700

    Create test for hangman function

--- test/test.c
+++ test/test.c
@@ -2,8 +2,14 @@
 
 #include "hangman.h"
 #include <ctest.h>
-
+void test_hangman()
+{
+    for (int i = 0; i <= 6; i++) {
+        hangman(i);
+    }
+}
 int main(int argc, const char** argv)
 {
+    test_hangman();
     return ctest_main(argc, argv);
 }
